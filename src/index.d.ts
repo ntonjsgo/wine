231df4a325c35539fd44ebcabb227ab817a5e427
Antonio Sgobio <asgobio.work@gmail.com>
2023-07-17 15:46:07 +0200

    created models

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,22 @@
+import { PriceList } from "@medusajs/medusa";
+
 export declare module "@medusajs/medusa/dist/models/product" {
     declare interface Product {
       year: number;
       alchool: number;
       grape: string;
     }
+    
   }
+
+export declare module "@medusajs/medusa/dist/models/sales-channel"{
+  declare interface SalesChannel{
+    price_lists: PriceList[]
+  }
+}
+
+export declare module "@medusajs/medusa/dist/models/price-list"{
+  declare interface PriceList{
+    sales_channel: SalesChannel
+  }
+}
